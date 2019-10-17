de61bba23d448f6d9738ab08b5d072bb6c8e9dcf
englerfa <englerfa@student.ethz.ch>
2019-10-17 14:23:07 +0200

    func added

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func sayHello(s string) {
+	fmt.Println("hello " + s)
+}
 
 func main() {
+	//v := Vertex{3, 4}
+	//fmt.Print("Abs() is ")
+	//fmt.Println(v.Abs())
 
+	sayHello("world")
 }
